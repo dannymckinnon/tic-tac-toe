b8017762a127b025cfdc37275b10bc13541c3dbb
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-05-19 14:00:05 -0700

    Create gameboard object

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+// gameboard object
 const gameBoard = (() => {
-  
+  const board = [
+    'x', 'o', 'x',
+    'o', 'x', 'o',
+    'x', 'o', 'x'
+  ]; 
   return {};
 })();
+
+// player factory
+
+
+// control flow of game object
+
+// display contents on webpage
